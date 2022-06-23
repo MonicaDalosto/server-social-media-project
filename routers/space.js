e0cc6092584a63df0fcac83452ee85d9a25eb194
MonicaDalosto <monica.kerber@gmail.com>
2022-06-23 11:07:17 +0200

    Feature 2 - Created a new endpoint to get the spaceById with the storie

--- routers/space.js
+++ routers/space.js
@@ -7,11 +7,22 @@ const router = new Router();
 // http -v localhost:4000/spaces
 router.get('/', async (request, response) => {
   try {
-    const allSpaces = await Space.findAll({ include: [Story] });
+    const allSpaces = await Space.findAll();
     console.log('all spaces: ', allSpaces);
     response.send(allSpaces);
   } catch (error) {
     console.log('error from all spaces: ', error.message);
+  }
+});
+
+router.get('/details/:id', async (request, response, next) => {
+  try {
+    const { id } = request.params;
+    const spaceById = await Space.findByPk(id, { include: [Story] });
+    response.send(spaceById);
+  } catch (error) {
+    console.log('error from space by id: ', error.message);
+    next(error);
   }
 });
 
