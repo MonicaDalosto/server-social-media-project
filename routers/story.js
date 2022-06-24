ab4da506a006b4027ce58923f932d300366fabde
MonicaDalosto <monica.kerber@gmail.com>
2022-06-24 17:08:42 +0200

    Created the endpoint to cerate a new Story

--- routers/story.js
+++ routers/story.js
@@ -14,6 +14,23 @@ router.get('/', async (request, response) => {
   }
 });
 
+// http -v POST :4000/stories name="Cool Story" content="Some cool story from Codaisseur final assesment on class 59" imageUrl="https://coursereport-production.imgix.net/uploads/school/logo/426/original/codaisseur-square.png?w=200&h=200&dpr=1&q=75" spaceId=1
+router.post('/', authMiddleware, async (request, response, next) => {
+  try {
+    const { name, content, imageUrl, spaceId } = request.body;
+    const newStory = await Story.create({
+      name,
+      content,
+      imageUrl,
+      spaceId
+    });
+    response.send(newStory);
+  } catch (error) {
+    console.log('error from Story router: ', error.message);
+    next(error);
+  }
+});
+
 router.delete('/:id', authMiddleware, async (request, response, next) => {
   try {
     const { id } = request.params; // the value is from the thunk;
