2749bfc94485e4e9e7c27f6d0e7eed2547c29bf4
MonicaDalosto <monica.kerber@gmail.com>
2022-06-25 14:58:16 +0200

    Feature 6: Done - Created an put endpoint to update the mySpace

--- routers/space.js
+++ routers/space.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const Space = require('../models/').space;
 const Story = require('../models/').story;
+const authMiddleware = require('../auth/middleware');
 
 const router = new Router();
 
@@ -36,6 +37,27 @@ router.post('/', async (request, response, next) => {
     response.send(newSpace);
   } catch (error) {
     console.log('error from mySpace: ', error.message);
+    next(error);
+  }
+});
+
+// http -v PUT :4000/spaces/4 title="Funny Stories" description="Just some funny stories of my life" backgroundColor="#422929" color="#eee2e2"
+router.put('/:id', authMiddleware, async (request, response, next) => {
+  try {
+    const { id } = request.params; // receive the space id from the thunks
+    const { title, description, backgroundColor, color } = request.body;
+
+    const mySpace = await Space.findByPk(id);
+
+    const mySpaceUpdated = await mySpace.update({
+      title,
+      description,
+      backgroundColor,
+      color
+    });
+    response.send(mySpaceUpdated);
+  } catch (error) {
+    console.log('error from Updating my Space endPoint: ', error.message);
     next(error);
   }
 });
